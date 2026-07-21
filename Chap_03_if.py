cf5af49d64241077a04504fc9a1035a9a9effd5e
반재영 <113581663+mycom0216@users.noreply.github.com>
2026-07-21 17:26:41 +0900

    Add files via upload

--- Chap_03_if.py
+++ Chap_03_if.py
@@ -82,6 +82,67 @@ pants = "파랑"
 if (hair == "빨강") or (pants == "파랑"):
     print("우리가 찾던 사람입니다.")
     
+# input() 입력값 반환 함수
+
+#user_input = input("1. 확인 / 2. 취소 번호를 선택해주세요 :")
+#
+#if user_input.isdigit():
+    #user_input = int(user_input)
+#else :
+    #print("입력을 잘못했습니다.")
+#print(user_input)
+
+#user_input = input("0-100사이의 정수를 입력해주세요")
+#user_num = int(user_input)
+
+#if 100 >= user_num > 90:
+    #print("A등급")
+#elif 90 >= user_num > 80:
+    #print("B등급")
+#elif 80 >= user_num > 70:
+    #print("C등급")
+#elif 70 >= user_num > 60:
+    #print("D등급")
+#else:
+    #print("F등급")
+    
+    #user_input = input("나이를 정수로 입력하세요.")
+    
+    #user_num = int(user_input)
+    
+    #if user_num < 13:
+        #print("5000원 내야합니다.")
+    #elif    user_num > 50:
+        #print("3000원 내야합니다.")
+    #else:
+        #print("10000원 내야합니다.")
+    
+    
+    
+    # 다중필터링: 성적 평가 시스템
+    scores = (80, 70, 100, 60)
+    avg = (scores[0] + scores[1] + scores[2] + scores[3] / 4) #avg : 에버리지의 줄임말
+    under = (scores[0] < 50) or (scores[1] < 50) or (scores[2] < 50) or (scores[3] < 50)
+    
+    if avg >= 60:
+        print("휴일 부여")
+    else:
+        print("휴일 없음")
+    if under:
+        print("보충 수업 있음")
+    else:
+        print("보충 수업 없음")
+    if (100 in scores) and (not under):
+        print("상장 수여")
+    else:
+        print("상장 없음")
+    
+    
+   
+    
+
+
+
 
 
 
