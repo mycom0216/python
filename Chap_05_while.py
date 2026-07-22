c5de374de1d89da6c1efb86b605652204cd9032c
반재영 <113581663+mycom0216@users.noreply.github.com>
2026-07-22 16:13:03 +0900

    Add files via upload

---
+++ Chap_05_while.py
@@ -0,0 +1,50 @@
+## for,while
+#n = 0
+
+#while n < 4:
+    #print("a")
+    #n += 1
+    
+#user_input = input("입력(종료시 '/q'): ")    
+    
+#while user_input!= "/q":
+    #print(user_input)
+    #user_input = input("입력(종료시 '/q'): ")
+    
+    
+    
+#user_input = input("입력(종료시 '/q'): ")
+    
+#while user_input!= "/q":
+    #if user_input.isdigit():
+        #print(f"{user_input}원입니다.")
+    #else:
+        #print(user_input)
+    #user_input = input("입력(종료시 '/q'): ")
+    
+    
+    # 1-10까지 1씩 증가하면서 출력하는 반복문
+    
+#num = 1
+#while num < 11:
+            #print(num)
+#num += 1
+       
+     
+       
+    # 10-0 까지 1씩 감소하면서 출력하는 반복문
+#num2 = 10
+#while num2 >= 0:
+        #print(num2)
+        #num2 -= 1  
+        
+        # 0부터 시작해서 2씩해서 20까지 출력하는 반복문
+num = 0
+while num < 21:         ## 증가
+            print(num)
+            num += 2
+        # 20부터 시작해서 4씩 줄어들어서 0까지 출력하는 반복문
+            num2 = 20
+            while num2 >= 0:    ## 감소
+                print(num2)
+                num2 -= 4
