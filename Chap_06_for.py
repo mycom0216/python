b63237ce6644fe2ac37c3b70374708babbc6ebb3
반재영 <113581663+mycom0216@users.noreply.github.com>
2026-07-23 11:13:58 +0900

    Add files via upload

---
+++ Chap_06_for.py
@@ -0,0 +1,50 @@
+#for i in range(5): #슬라이싱, range = 시작값,끝값,증가폭
+    #print(i)             
+#print(list(range(10)))               
+#for i in range(2,5): 
+    #print(i)
+    
+#print(list(range(2,5)))
+
+#print(list(range(0,5,2)))
+
+
+                
+        
+     
+    
+        
+#a = 9 #별찍기
+#for i in range(a):
+    #print("" * (a - (i+1)), end="")
+    #print('*' * (i+1))
+            
+#output = "" #시작
+#for i in range(1,10):
+                #for j in range(0,i):
+                    #output += "*"
+                    #output += "\n"
+                    #print(output) #끝
+                    
+                    #output = ""
+                    #for i in range(1,15):
+                        #for j in range(14,i,-1):
+                            #output += ''
+                            #for  j in range(0,2,* i -1):
+                                #output += '*'
+                                #output += '\n'
+                                #print(output)
+
+#for i in range(5):
+    #print(i)
+    
+    #output = ""
+#for i in range(5,10):
+        #for j in range(14,i,-1):
+            #output += ''
+            #output += "*"
+            #output += "\n"
+            #print(output)
+
+for i in range(10):
+    print("안녕하세요")
