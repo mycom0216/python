bc464a010600ae32298e50b185421eadffe4a6a4
반재영 <113581663+mycom0216@users.noreply.github.com>
2026-07-23 15:00:07 +0900

    Add files via upload

--- Chap_06_for.py
+++ Chap_06_for.py
@@ -46,5 +46,90 @@
             #output += "\n"
             #print(output)
 
-for i in range(10):
-    print("안녕하세요")
+#for i in range(10):
+    #print("안녕하세요")
+    
+    #13인의아해가도로로질주하오
+    #(길은막다른골목이적당하오)
+    
+    #제1의아해가무섭다고그리오.
+    #제1의아해도무섭다고그리오.
+    #제1의아해도무섭다고그리오.
+    #제1의아해도무섭다고그리오.
+    #제1의아해도무섭다고그리오.
+    #제1의아해도무섭다고그리오.
+    #제1의아해도무섭다고그리오.
+    #제1의아해도무섭다고그리오.
+    #제1의아해도무섭다고그리오.
+    #제1의아해도무섭다고그리오.
+    #제1의아해도무섭다고그리오.
+    #제1의아해도무섭다고그리오.
+    #제1의아해도무섭다고그리오.
+    
+#print("13인의아해가도로로질주하오.\n(길은막다른골목이적당하오)\n\n제1의아해가무섭다고그리오.")
+
+#for i in range(2,14):
+    #print(f"제[i]의아해도무섭다고그리오.")
+    
+#target = 2
+#for i in range(1,10):
+    #print(f"{target} X {target * i} = i")
+    
+#for i in range(3):
+        #for j in range(3):
+            #print("안녕하세요")
+            
+#li = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+#target = 4
+#idx = 0
+#switch = 1
+
+#for i in range(3):
+    #if switch == 0:
+        #break
+    #for j in range(3):
+        #if li[i][j] == target:
+            #print(f"타겟의 전체에서의 인덱스는 {idx}입니다.")
+            #switch = 0 
+            #break
+        #print("타겟이 아닙니다")
+        #idx += 1 
+        
+#size = 2
+#for i in range(size):
+    #for j in range(size):
+        #print("*",end="")
+    #print()
+        
+
+#size = 6 #삼각형
+#for i in range(size):
+    #for j in range(i+1):
+           #print('*',end="")
+    #print()
+# print("------------------------------")
+#size = 5    #역삼각형
+#for i in range(size):
+        #for j in range(size-i):
+            #print('*', end="")
+        #print()
+        
+size =5
+
+for i in range(size):
+    for j in range(size - i - 1): ## 띄어쓰기 간격 중요
+        print(" ", end="") ##"" >> 띄어쓰기 매우중요함(띄어쓰기에따라서 위치바뀜)
+    for k in range(i + 1): ## 여기도 + >> 사이 띄어쓰기
+        print("*", end="") ## 띄어쓰기
+    print()
+
+    
+    
+        
+        
+        
+        
+        
+        
+    
+   
